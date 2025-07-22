6e0efbed63876d1c3205ef2f2957c24db2a7e4c0
koyebadvanced <koyebadvanced@gmail.com>
2025-07-22 09:59:10 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,7 +7,7 @@ API_ID = int(environ.get("API_ID", "21601817"))
 API_HASH = environ.get("API_HASH", "8d0fe8b5ae8149455681681253b2ef17")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "6764661699"))
+OWNER = int(environ.get("OWNER", "6705556585"))
 CREDIT = environ.get("CREDIT", "[𝐖𝐀𝐑𝐑𝐈𝐎𝐑](https://t.me/staystrongbros)")
 
 
